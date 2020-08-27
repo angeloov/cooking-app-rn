1f99cd0f68a04758d6b5c8d736c9bb37e02c81f9
angeloov <voicuangelo04@gmail.com>
2020-08-27 16:46:19 +0200

    Added video component
    Signed-off-by: angeloov <voicuangelo04@gmail.com>

--- components/screens/VideoScreen.js
+++ components/screens/VideoScreen.js
@@ -5,14 +5,16 @@ import Video from 'react-native-video';
 export default function VideoScreen() {
   return (
     <View>
-      <Video
-        source={{ uri: 'https://www.w3schools.com/html/mov_bbb.mp4' }}
-        shouldPlay
-        useNativeControls
-        style={{
-           width: "500%", height: "50%"
-        }}
-      />
+        <Video
+          source={{ uri: 'https://banzai-pmd-meride-tv.akamaized.net/amets/giallozafferano/2019/12/oosyer/HD/1200_oosyer.m3u8', type:"m3u8" }}
+          ignoreSilentSwitch="ignore"
+          controls={true}
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+          volume={1.0}
+        />
     </View>
   );
 }
